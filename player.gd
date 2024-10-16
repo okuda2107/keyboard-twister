dbb6d62a3f93ddec081062c253b62cbd1c2cce14
okuda <okuda2107@gmail.com>
2024-10-17 01:05:57 +0900

    wip

---
+++ player.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_T:
+			print("T was pressed")
